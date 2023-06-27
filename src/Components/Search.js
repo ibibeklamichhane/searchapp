aadeeb26d359aa321b2a74da8abc1fe826704e21
ibibeklamichhane <bibeklc714@gmail.com>
2023-06-27 12:21:41 +0545

    basic searchbar added

---
+++ src/Components/Search.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import Table from "./Table";
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const [products, setProducts] = useState([]);
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+
+
+  const searchProducts = async () => {
+    
+    try {
+     //Api  from fakesite   
+      const response = await fetch(`https://fakestoreapi.com/products?q=${searchTerm}`);
+      const data = await response.json();
+      console.log('search',data);
+      setProducts(data);
+    } catch (error) {
+      console.error('Error searching products:', error);
+    }
+  };
+
+  //removing word sensitivity by converting into lower case
+  const Products = products.filter((product) =>
+  product.title.toLowerCase().includes(searchTerm.toLowerCase())
+);
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search products..."
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+
+
+
+      <button onClick={searchProducts}>Search</button>
+
+      <ul>
+        {Products.map((product) => (
+          <li key={product.id}>{product.price}</li>
+          
+
+
+        ))}
+      </ul> 
+    </div>
+  );
+};
+
+
+export default SearchBar;
+
+
+ {/*    <ul>
+        {filteredProducts.map((product) => (
+          <li key={product.id}>{product.price}</li>
+          
+
+
+        ))}
+      </ul> */}
